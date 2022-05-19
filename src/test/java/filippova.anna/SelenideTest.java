8ce76a32b69de6459832b5c9de7b60e9ff9a461e
Anna Filippova <anna.filippova.8@yandex.ru>
2022-05-19 13:25:32 +0300

    added Allure tests

---
+++ src/test/java/filippova.anna/SelenideTest.java
@@ -0,0 +1,30 @@
+package filippova.anna;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testGithubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("AnnaFilippovaa/parametrized_test");
+        $(".header-search-input").submit();
+
+        $(linkText("AnnaFilippovaa/parametrized_test")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#1")).should(Condition.visible);
+    }
+}
